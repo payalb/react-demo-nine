f7581527d32ef6ba9b508f67c9794c69c4b2ff36
unknown <jahanvi.bansal@gmail.com>
2023-03-26 00:24:33 +0530

    Initialize project using Create React App

---
+++ src/Test1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Test1 extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Test1
